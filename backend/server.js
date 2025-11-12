be8d698f0fdcfa658b0b7efecf399de149ded04e
csierra988 <csierra@vt.edu>
2025-11-12 01:39:07 -0500

    Added images for the tarot cards, created the server for the backend using express

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+//import routes after adding them in later
+
+//initializing the app
+const app = express();
+
+//maybe add authentication later
+
+app.use(cors()); //frontend requests
+app.use(express.json()); //parsing json requests
+
+//testing a basic request route on the root url
+app.get('/', (req, res) => {
+    res.send("Kippy's Tarot backend server is running");
+});
+
+//starting the server
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
