76523ba95e5e45c66a69938289913130fecc862b
csierra988 <csierra@vt.edu>
2025-12-02 22:23:08 -0500

    started working on frontend login/signup

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,21 @@
 //the home page - welcomes user and has the start reading button
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    width: 100vw;
+`;
 
 function Home() {
 
     return (
-        <div>
+        <Wrapper>
             this is the home page
-        </div>
+        </Wrapper>
     );
 }
 
