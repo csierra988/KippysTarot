221717066e8167d0f3591b55f28c1788022dddd5
csierra988 <csierra@vt.edu>
2025-12-04 16:17:26 -0500

    fixed issue where if a card was pulled back to back, it would return on the front image

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -13,6 +13,51 @@ const Wrapper = styled.div`
     width: 100vw;
 `;
 
+const Content = styled.div`
+    display: flex;
+    width: 100%;
+    gap: 20px;
+    margin-top: 60px;
+`;
+
+const KippyBlock = styled.div`
+    display: flex;
+    flex: 1;
+    justify-content: center;
+    background: black;
+    min-height: 400px;
+
+    @media (max-width: 888px) {
+        display: none;
+    }
+`;
+
+const Text = styled.div`
+    align-items: center;
+    background-color: white;
+    border: 2px solid black;
+    width: 500px;
+    height: 50px;
+    padding: 10px;
+    color: black;
+`;
+
+const Container = styled.div`
+    flex: 2;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 50px;
+    padding: 10px;
+`;
+
+const ReadingOptions = styled.div`
+`;
+
+const ReadingButtons = styled.button`
+`;
+
 function Home() {
     const [user, setUser] = useState(null);
     const auth = getAuth();
@@ -30,8 +75,18 @@ function Home() {
 
     return (
         <Wrapper>
-            <p>hi {user?.displayName || ""}!</p>
-            <p>this is the home page</p>
+            <Content>
+                <KippyBlock>
+                    <p>meow its me kippy</p>
+                </KippyBlock>
+
+                <Container>
+                    <Text>
+                         hi {user?.displayName || ""}!
+                        this is the home page
+                    </Text>
+                </Container>
+            </Content>
         </Wrapper>
     );
 }
